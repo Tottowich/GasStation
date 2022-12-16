e07d8c91fe1fae0b6e4323d12af14caef491c091
Jonsson <tfy21tjn@ad.cs.umu.se>
2022-12-16 12:28:22 +0100

    Gettting there!

--- Code/testspace.m
+++ Code/testspace.m
@@ -1,6 +1,21 @@
 %%
-gasStation(100,1,1000)
-
+buy_price = 15.0;
+sell_price = 20.0;
+n_pumps = 4;
+T = 100;
+tank_size = 100000;
+customer_rate = 20;
+pump_que_cap = 2;
+profit = gasStation(T,n_pumps,buy_price,sell_price,tank_size,customer_rate,pump_que_cap)
+%%
+N = 100;
+profits = zeros(N,1);
+for i=1:N
+    fprintf("Simulation: %d",i)
+    profits(i) = gasStation(T,n_pumps,buy_price,sell_price,tank_size,customer_rate,pump_que_cap);
+end
+%%
+mean(profits)
 %% Testing hungry probabilities
 N = 100000; % Arriving cars.
 Ps = 1:6;

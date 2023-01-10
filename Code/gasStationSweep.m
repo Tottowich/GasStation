e45c0123f89c4584a8c474bfff7167d83cd57b21
Tottowich <78850225+Tottowich@users.noreply.github.com>
2023-01-10 11:43:25 +0100

    Sweep complete

--- Code/gasStationSweep.m
+++ Code/gasStationSweep.m
@@ -1,9 +1,9 @@
 %% Sweeping should occure here.
 buy_price = 18.0;
 sell_price = 20.0;
-n_pumps = 8:20;
+n_pumps = 4:20;
 T = 7*24;
-tank_size = (5:0.25:6)*10^5;
+tank_size = (4:0.25:8)*10^5;
 customer_rate = 150;
 pump_que_cap = 2;
 pump_hourly_cost = 50;%2250W => 
@@ -18,7 +18,8 @@ pos(4) = 80;
 set(f,'Position', pos);
 total_iter = iters*nt*np;
 count = 0;
-times = [];
+% times = [];
+load_system("StartBlock.slx")
 for t = 1:nt
     for n = 1:np
         temps = zeros(iters,1);
@@ -28,12 +29,12 @@ for t = 1:nt
             signals = gasStationSimulation ...
                 (T,n_pumps(n),tank_size(t),customer_rate,pump_que_cap,pump_rate);
             temps(i) = gasStationProfits(signals,T,n_pumps(n),buy_price,sell_price,pump_hourly_cost,pump_rate);
-            time = toc;
-            times = [times;time];
-            process_time = mean(times);
-            left = total_iter-count;
-            time_left = process_time*left;
-            waitbar(round(count/total_iter,3),f,"Time remaining: "+round(time_left,3)+newline+"Tank Size: ("+t+"/"+nt+")."+newline+"Number pumps: ("+n+"/"+np+")."+newline+"Iteration: ("+i+"/"+iters+")")
+%             time = toc;
+%             times = [times;time];
+%             process_time = mean(times);
+%             left = total_iter-count;
+%             time_left = process_time*left;
+            waitbar(round(count/total_iter,3),f,"Tank Size: ("+t+"/"+nt+")."+newline+"Number pumps: ("+n+"/"+np+")."+newline+"Iteration: ("+i+"/"+iters+")")
             if isnan(temps(i))
                 temps(i:end) = NaN;
                 count = count + np-i;
@@ -48,7 +49,6 @@ close(f)
 %% 3D Plotting
 [X,Y] = meshgrid(tank_size,n_pumps);
 surf(X,Y,profits');
-
 xlabel('Tank size')
 ylabel('Number of pumps')
 zlabel('Profit')
